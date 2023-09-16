bd62f7ada4513b0d2873296ca75bb242c02e5fc5
ka <todo1@rotarydef.net>
2023-09-15 21:29:10 -0700

    first

--- create_vm/create_vm.cc
+++ create_vm/create_vm.cc
@@ -156,38 +156,3 @@ extern "C" void* extern_initialize(){ return new kautil::jni::JVM{}; }
 extern "C" void extern_finalize(void * instance){  delete reinterpret_cast<kautil::jni::JVM*>(instance); }
 
 
-//
-//
-//int tmain_create_vm(const char * directory_of_system_jars/*void *(*dlopen)(const char *,int),void *(*dlsym)(void *,const char *),int(*dlclose)(void *)*/,int dlop){
-//    // one instance per process
-//    auto vm=kautil::jni::java_vm();{
-//        try{
-//            printf("set up java vm info\n");fflush(stdout);
-//            vm.Xms("1024m");
-//            vm.Xmx("2048m");
-//            vm.add_java_class_path_with_directory(directory_of_system_jars);
-//            printf("create java vm\n"); fflush(stdout);
-////            vm.create_once(dlopen,dlsym,dlclose,dlop);
-//            vm.create_once(dlop);
-//            printf("created java vm successfully\n");fflush(stdout);
-//        }catch(...){
-//            printf("error");
-//        }
-//    }
-//    
-//    auto env = vm.env(); 
-//    auto jstr = env->NewStringUTF("this is jstring");
-//    if(env->ExceptionOccurred()) env->ExceptionDescribe();
-//    
-//    auto cls = env->FindClass("java/lang/System");
-//    auto cstr = env->GetStringUTFChars((jstring)jstr,nullptr);
-//    printf("%s\n",cstr); fflush(stdout);
-//    env->ReleaseStringUTFChars(jstr,cstr);
-//    
-//    
-//    env->DeleteLocalRef(jstr);
-//    return 0;
-//}
-
-
-
